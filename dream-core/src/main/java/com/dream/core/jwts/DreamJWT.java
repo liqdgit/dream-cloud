10361ee7099cc944f9e65b4d3c0b0ba9242ae055
liqingdong <liqd163@163.com>
2018-01-05 17:41:15 +0800

    jwt

---
+++ dream-core/src/main/java/com/dream/core/jwts/DreamJWT.java
@@ -0,0 +1,49 @@
+package com.dream.core.jwts;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * <p>Title:      DreamJWT. </p>
+ * <p>Description JWT token </p>
+ *
+ * @author         <a href="liqd163@163.com"/>李清栋</a>
+ * @CreateDate     2018/1/5 17:18
+ */
+public class DreamJWT {
+
+    /**
+     * <p>Title:      创建 jwt token. </p>
+     * <p>Description </p>
+     *
+     * @param         
+     * @author        <a href="liqd163@163.com"/>李清栋</a>
+     * @CreateDate    2018/1/5 17:32
+     * @return        
+     */
+    public String createJWT(DreamToken dreamToken){
+        String token = Jwts.builder()
+                .setSubject(dreamToken.getSubject())
+                .claim(dreamToken.getDataKey(), dreamToken.getData())
+                .setIssuedAt(dreamToken.getNow())
+                .setExpiration(dreamToken.getExp())
+                .signWith(SignatureAlgorithm.HS256, dreamToken.getTokenKey()).compact();
+        return token;
+    }
+
+    /**
+     * <p>Title:      解密 jwt token. </p>
+     * <p>Description </p>
+     *
+     * @param         
+     * @author        <a href="liqd163@163.com"/>李清栋</a>
+     * @CreateDate    2018/1/5 17:32
+     * @return        
+     */
+    public Claims parseJWT(String tokenKey, String token){
+        final Claims claims = Jwts.parser().setSigningKey(tokenKey).parseClaimsJws(token).getBody();
+        return claims;
+    }
+}
---
+++ dream-core/src/main/java/com/dream/core/jwts/DreamToken.java
@@ -0,0 +1,49 @@
+package com.dream.core.jwts;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>Title:      DreamToken. </p>
+ * <p>Description JWT token </p>
+ *
+ * @author         <a href="liqd163@163.com"/>李清栋</a>
+ * @CreateDate     2018/1/5 17:10
+ */
+@Data
+public class DreamToken implements Serializable {
+
+    private static final long serialVersionUID = 6276470016107496721L;
+
+    /**
+     * id
+     */
+    private String subject;
+
+    /**
+     * 数据key
+     */
+    private String dataKey;
+
+    /**
+     * data
+     */
+    private String data;
+
+    /**
+     * 签发时间
+     */
+    private Date now;
+
+    /**
+     * 过期时间
+     */
+    private Date exp;
+
+    /**
+     * 私钥
+     */
+    private String tokenKey;
+}
